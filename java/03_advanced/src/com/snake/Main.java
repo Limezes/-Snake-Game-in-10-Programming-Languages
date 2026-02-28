58689e13b30578ef62e48a911be9bfbb6c551d1a
✞Lime✞ <z6nqhtw7nm@privaterelay.appleid.com>
2026-02-28 21:28:55 +0300

    Update Main.java

--- java/03_advanced/src/com/snake/Main.java
+++ java/03_advanced/src/com/snake/Main.java
@@ -1,1 +1,36 @@
+package com.snake;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.text.Font;
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Загрузка шрифтов
+        Font.loadFont(getClass().getResourceAsStream("/fonts/PressStart2P.ttf"), 14);
+        
+        // Загрузка главного окна
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainMenu.fxml"));
+        Parent root = loader.load();
+        
+        primaryStage.setTitle("Snake Game - JavaFX");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+        
+        // Обработка закрытия
+        primaryStage.setOnCloseRequest(e -> {
+            GameController controller = loader.getController();
+            controller.shutdown();
+        });
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
