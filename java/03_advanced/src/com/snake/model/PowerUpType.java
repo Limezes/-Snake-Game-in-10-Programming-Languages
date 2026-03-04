ec5aca9caef441bce0205e3afa27b9305f54eab3
✞Lime✞ <z6nqhtw7nm@privaterelay.appleid.com>
2026-03-04 23:47:58 +0300

    Update PowerUpType.java

--- java/03_advanced/src/com/snake/model/PowerUpType.java
+++ java/03_advanced/src/com/snake/model/PowerUpType.java
@@ -1,1 +1,33 @@
+package com.snake.model;
 
+import javafx.scene.paint.Color;
+import java.util.Random;
+
+public enum PowerUpType {
+    SPEED_BOOST("⚡", Color.CYAN),
+    INVINCIBILITY("🛡️", Color.GOLD),
+    SLOW_TIME("⏱️", Color.PURPLE),
+    EXTRA_LIFE("❤️", Color.RED);
+    
+    private final String symbol;
+    private final Color color;
+    private static final Random random = new Random();
+    
+    PowerUpType(String symbol, Color color) {
+        this.symbol = symbol;
+        this.color = color;
+    }
+    
+    public String getSymbol() {
+        return symbol;
+    }
+    
+    public Color getColor() {
+        return color;
+    }
+    
+    public static PowerUpType getRandom() {
+        PowerUpType[] types = values();
+        return types[random.nextInt(types.length)];
+    }
+}
