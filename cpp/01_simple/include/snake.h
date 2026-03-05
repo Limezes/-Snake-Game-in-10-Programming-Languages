09fef1da2ce535abde6ba4c50adbef41da2aa61a
✞Lime✞ <z6nqhtw7nm@privaterelay.appleid.com>
2026-03-06 02:39:05 +0300

    Update snake.h

--- cpp/01_simple/include/snake.h
+++ cpp/01_simple/include/snake.h
@@ -1,1 +1,32 @@
+#ifndef SNAKE_H
+#define SNAKE_H
 
+#include <deque>
+#include <utility>
+
+class SnakeGame {
+private:
+    static const int WIDTH = 20;
+    static const int HEIGHT = 10;
+    static const int INITIAL_SPEED = 150;
+    
+    std::deque<std::pair<int, int>> snake;
+    std::pair<int, int> food;
+    enum Direction { UP, DOWN, LEFT, RIGHT } dir, nextDir;
+    int score;
+    bool gameOver;
+    bool running;
+    
+    void createFood();
+    void move();
+    void render();
+    void handleInput(char key);
+    void clearScreen();
+    
+public:
+    SnakeGame();
+    void reset();
+    void start();
+};
+
+#endif // SNAKE_H
