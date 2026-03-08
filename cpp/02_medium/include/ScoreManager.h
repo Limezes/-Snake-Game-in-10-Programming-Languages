8656f1ed4c91c30afa05002f1333d54585b168f9
✞Lime✞ <z6nqhtw7nm@privaterelay.appleid.com>
2026-03-08 11:54:12 +0300

    Update ScoreManager.h

--- cpp/02_medium/include/ScoreManager.h
+++ cpp/02_medium/include/ScoreManager.h
@@ -1,1 +1,25 @@
+#ifndef SCORE_MANAGER_H
+#define SCORE_MANAGER_H
 
+#include <string>
+
+class ScoreManager {
+private:
+    int currentScore;
+    int highScore;
+    std::string playerName;
+    
+    void loadHighScore();
+    void saveHighScore();
+    
+public:
+    ScoreManager();
+    
+    void addScore(int points);
+    void resetScore();
+    int getCurrentScore() const { return currentScore; }
+    int getHighScore() const { return highScore; }
+    void setPlayerName(const std::string& name) { playerName = name; }
+};
+
+#endif // SCORE_MANAGER_H
