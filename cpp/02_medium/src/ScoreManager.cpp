c0fef632800b2281a6b76de716c70751408e1d53
✞Lime✞ <z6nqhtw7nm@privaterelay.appleid.com>
2026-03-09 20:34:49 +0300

    Update ScoreManager.cpp

--- cpp/02_medium/src/ScoreManager.cpp
+++ cpp/02_medium/src/ScoreManager.cpp
@@ -1,1 +1,35 @@
+#include "../include/ScoreManager.h"
+#include <fstream>
+#include <iostream>
 
+ScoreManager::ScoreManager() : currentScore(0), highScore(0) {
+    loadHighScore();
+}
+
+void ScoreManager::loadHighScore() {
+    std::ifstream file("highscore.txt");
+    if (file.is_open()) {
+        file >> highScore;
+        file.close();
+    }
+}
+
+void ScoreManager::saveHighScore() {
+    std::ofstream file("highscore.txt");
+    if (file.is_open()) {
+        file << highScore;
+        file.close();
+    }
+}
+
+void ScoreManager::addScore(int points) {
+    currentScore += points;
+    if (currentScore > highScore) {
+        highScore = currentScore;
+        saveHighScore();
+    }
+}
+
+void ScoreManager::resetScore() {
+    currentScore = 0;
+}
