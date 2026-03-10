7bd9b01877336bba39d0f45d1ac89fad7f2cb735
✞Lime✞ <z6nqhtw7nm@privaterelay.appleid.com>
2026-03-10 14:20:48 +0300

    Update Wall.h

--- cpp/03_advanced/include/Wall.h
+++ cpp/03_advanced/include/Wall.h
@@ -1,1 +1,22 @@
+#ifndef WALL_H
+#define WALL_H
 
+#include <QPoint>
+#include <QColor>
+
+class Wall {
+private:
+    QPoint position;
+    QColor color;
+    bool destructible;
+
+public:
+    Wall(int x, int y, bool destructible = false);
+    
+    QPoint getPosition() const { return position; }
+    QColor getColor() const { return color; }
+    bool isDestructible() const { return destructible; }
+    void setDestructible(bool d) { destructible = d; }
+};
+
+#endif // WALL_H
