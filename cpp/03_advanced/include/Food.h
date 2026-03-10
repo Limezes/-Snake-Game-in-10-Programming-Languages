cfdf349553f42fea1c00b3d3ec74a75aa03fc28a
✞Lime✞ <z6nqhtw7nm@privaterelay.appleid.com>
2026-03-10 14:20:21 +0300

    Update Food.h

--- cpp/03_advanced/include/Food.h
+++ cpp/03_advanced/include/Food.h
@@ -1,1 +1,41 @@
+#ifndef FOOD_H
+#define FOOD_H
 
+#include <QPoint>
+#include <QColor>
+
+enum class FoodType {
+    NORMAL,
+    BONUS,
+    SPEED,
+    SLOW,
+    GOLDEN
+};
+
+class Food {
+private:
+    QPoint position;
+    FoodType type;
+    int points;
+    QColor color;
+    int lifetime;
+    int age;
+    
+public:
+    Food();
+    
+    void respawn(const QVector<QPoint>& occupiedPositions);
+    void update();
+    bool isExpired() const;
+    
+    // Геттеры
+    QPoint getPosition() const { return position; }
+    FoodType getType() const { return type; }
+    int getPoints() const { return points; }
+    QColor getColor() const { return color; }
+    
+    // Сеттеры
+    void setPosition(const QPoint& pos) { position = pos; }
+};
+
+#endif // FOOD_H
