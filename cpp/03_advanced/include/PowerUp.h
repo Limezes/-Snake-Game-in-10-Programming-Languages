6e9fbaaa4d981e0738a29fa746d9ac9a264a780e
✞Lime✞ <z6nqhtw7nm@privaterelay.appleid.com>
2026-03-10 14:21:20 +0300

    Update PowerUp.h

--- cpp/03_advanced/include/PowerUp.h
+++ cpp/03_advanced/include/PowerUp.h
@@ -1,1 +1,38 @@
+#ifndef POWERUP_H
+#define POWERUP_H
 
+#include <QPoint>
+#include <QColor>
+
+enum class PowerUpType {
+    SPEED_BOOST,
+    INVINCIBILITY,
+    SLOW_TIME,
+    EXTRA_LIFE,
+    SCORE_MULTIPLIER
+};
+
+class PowerUp {
+private:
+    QPoint position;
+    PowerUpType type;
+    QColor color;
+    int duration;
+    int lifetime;
+    int age;
+    
+public:
+    PowerUp();
+    
+    void spawn(const QVector<QPoint>& occupiedPositions);
+    void update();
+    bool isExpired() const;
+    
+    // Геттеры
+    QPoint getPosition() const { return position; }
+    PowerUpType getType() const { return type; }
+    QColor getColor() const { return color; }
+    int getDuration() const { return duration; }
+};
+
+#endif // POWERUP_H
