e1c43060593ccdda0cd72a6e3b78051a683dbff2
✞Lime✞ <z6nqhtw7nm@privaterelay.appleid.com>
2026-03-11 18:27:49 +0300

    Update PowerUp.cpp

--- cpp/03_advanced/src/PowerUp.cpp
+++ cpp/03_advanced/src/PowerUp.cpp
@@ -1,1 +1,50 @@
+#include "../include/PowerUp.h"
+#include <QRandomGenerator>
 
+PowerUp::PowerUp() : duration(0), lifetime(300), age(0) {}
+
+void PowerUp::spawn(const QVector<QPoint>& occupiedPositions) {
+    int maxAttempts = 100;
+    
+    do {
+        position.rx() = QRandomGenerator::global()->bounded(20);
+        position.ry() = QRandomGenerator::global()->bounded(20);
+    } while (occupiedPositions.contains(position) && maxAttempts-- > 0);
+    
+    // Случайный тип power-up
+    int r = QRandomGenerator::global()->bounded(5);
+    type = static_cast<PowerUpType>(r);
+    
+    switch (type) {
+        case PowerUpType::SPEED_BOOST:
+            color = QColor(0, 255, 255);
+            duration = 500;
+            break;
+        case PowerUpType::INVINCIBILITY:
+            color = QColor(255, 255, 0);
+            duration = 500;
+            break;
+        case PowerUpType::SLOW_TIME:
+            color = QColor(255, 0, 255);
+            duration = 500;
+            break;
+        case PowerUpType::EXTRA_LIFE:
+            color = QColor(255, 100, 100);
+            duration = 0;
+            break;
+        case PowerUpType::SCORE_MULTIPLIER:
+            color = QColor(255, 200, 0);
+            duration = 0;
+            break;
+    }
+    
+    age = 0;
+}
+
+void PowerUp::update() {
+    age++;
+}
+
+bool PowerUp::isExpired() const {
+    return age >= lifetime;
+}
