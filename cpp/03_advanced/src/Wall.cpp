88b71c1a7cb28b3582e94f60f2809cf646a4c8c2
✞Lime✞ <z6nqhtw7nm@privaterelay.appleid.com>
2026-03-11 18:25:43 +0300

    Update Wall.cpp

--- cpp/03_advanced/src/Wall.cpp
+++ cpp/03_advanced/src/Wall.cpp
@@ -1,1 +1,11 @@
+#include "../include/Wall.h"
 
+Wall::Wall(int x, int y, bool destructible) 
+    : position(x, y), destructible(destructible) {
+    
+    if (destructible) {
+        color = QColor(150, 100, 50);
+    } else {
+        color = QColor(100, 100, 120);
+    }
+}
