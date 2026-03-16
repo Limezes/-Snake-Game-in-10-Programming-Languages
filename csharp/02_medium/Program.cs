c421c8b7ee8fee68d9e3e6271255c72a6241cd85
✞Lime✞ <z6nqhtw7nm@privaterelay.appleid.com>
2026-03-16 20:39:13 +0300

    Update Program.cs

--- csharp/02_medium/Program.cs
+++ csharp/02_medium/Program.cs
@@ -1,1 +1,15 @@
+using System;
+using System.Windows.Forms;
 
+namespace SnakeGameWinForms
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
+        }
+    }
+}
