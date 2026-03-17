dfd6febd4e439baba3fc0400ba5ca3b47659a4fb
✞Lime✞ <z6nqhtw7nm@privaterelay.appleid.com>
2026-03-17 17:33:29 +0300

    Update App.xaml.cs

--- csharp/03_advanced/SnakeGame/App.xaml.cs
+++ csharp/03_advanced/SnakeGame/App.xaml.cs
@@ -1,1 +1,43 @@
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using SnakeGame.Services;
+using SnakeGame.ViewModels;
 
+namespace SnakeGame
+{
+    public partial class App : Application
+    {
+        private readonly ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IDatabaseService, DatabaseService>();
+            services.AddSingleton<ISoundService, SoundService>();
+            services.AddSingleton<ISettingsService, SettingsService>();
+            
+            services.AddTransient<MainViewModel>();
+            services.AddTransient<GameViewModel>();
+            services.AddTransient<SettingsViewModel>();
+            
+            services.AddSingleton<MainWindow>();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            var mainViewModel = _serviceProvider.GetRequiredService<MainViewModel>();
+            
+            mainWindow.DataContext = mainViewModel;
+            mainWindow.Show();
+        }
+    }
+}
