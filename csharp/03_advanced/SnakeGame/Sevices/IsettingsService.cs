b353d98131ae16d0dc6e52a3507a57dca28e96d6
✞Lime✞ <z6nqhtw7nm@privaterelay.appleid.com>
2026-03-17 17:44:15 +0300

    Update IsettingsService.cs

--- csharp/03_advanced/SnakeGame/Sevices/IsettingsService.cs
+++ csharp/03_advanced/SnakeGame/Sevices/IsettingsService.cs
@@ -1,1 +1,31 @@
+using SnakeGame.Models;
 
+namespace SnakeGame.Services
+{
+    public interface ISettingsService
+    {
+        Difficulty GetDifficulty();
+        void SetDifficulty(Difficulty difficulty);
+
+        string GetPlayerName();
+        void SetPlayerName(string name);
+
+        bool IsSoundEnabled();
+        void SetSoundEnabled(bool enabled);
+
+        int GetSoundVolume();
+        void SetSoundVolume(int volume);
+
+        bool IsGridVisible();
+        void SetGridVisible(bool visible);
+
+        int GetHighScore();
+        void SetHighScore(int score);
+
+        int GetGameSpeed();
+        
+        void ResetToDefaults();
+        void Save();
+        void Load();
+    }
+}
