17a850137c1e2a92f6da131b82df1a03d66d1952
✞Lime✞ <z6nqhtw7nm@privaterelay.appleid.com>
2026-03-17 17:41:44 +0300

    Update Wall.cs

--- csharp/03_advanced/SnakeGame/Models/Wall.cs
+++ csharp/03_advanced/SnakeGame/Models/Wall.cs
@@ -1,1 +1,18 @@
+namespace SnakeGame.Models
+{
+    public class Wall
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool IsDestructible { get; set; }
+        public string Color { get; set; }
 
+        public Wall(int x, int y, bool destructible = false)
+        {
+            X = x;
+            Y = y;
+            IsDestructible = destructible;
+            Color = destructible ? "#8B4513" : "#4A4A5A";
+        }
+    }
+}
